7d5be45da156a29547ef9ad1e73ec7c95f346f25
Adria Carrasquilla <carrasquilla.adria@gmail.com>
2022-07-20 21:07:38 +0200

    Fixed color missmatch in tmux

--- nvim/.config/nvim/lua/settings/init.lua
+++ nvim/.config/nvim/lua/settings/init.lua
@@ -31,5 +31,7 @@ set.title = true
 vim.cmd([[
     let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
     let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+    set termguicolors
+    hi! Normal ctermbg=none ctermfg=none guifg=none guibg=none
 ]])
 set.termguicolors = true
