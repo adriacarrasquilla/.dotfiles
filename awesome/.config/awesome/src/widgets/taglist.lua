a58b8919b410446372e835831685fb562f4999da
Adri <carrasquilla.adria@gmail.com>
2022-10-25 19:18:28 +0200

    attempt to fix taglist click issue (failed)

--- awesome/.config/awesome/src/widgets/taglist.lua
+++ awesome/.config/awesome/src/widgets/taglist.lua
@@ -169,62 +169,81 @@ local list_update = function(widget, buttons, label, data, objects)
   end
 end
 
+local taglist_buttons = awful.util.table.join(
+  awful.button({ }, 1, awful.tag.viewonly),
+  awful.button({ modkey }, 1, awful.client.movetotag),
+  awful.button({ }, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, awful.client.toggletag),
+  awful.button({ }, 4, awful.tag.viewnext),
+  awful.button({ }, 5, awful.tag.viewprev)
+)
+
 return function(s)
-  return awful.widget.taglist(
-    s,
-    awful.widget.taglist.filter.noempty,
-    gears.table.join(
-      awful.button(
-        {},
-        1,
-        function(t)
-          t:view_only()
-        end
-      ),
-      awful.button(
-        { modkey },
-        1,
-        function(t)
-          if client.focus then
-            client.focus:move_to_tag(t)
-          end
-        end
-      ),
-      awful.button(
-        {},
-        3,
-        function(t)
-          if client.focus then
-            client.focus:toggle_tag(t)
-          end
-        end
-      ),
-      awful.button(
-        { modkey },
-        3,
-        function(t)
-          if client.focus then
-            client.focus:toggle_tag(t)
-          end
-        end
-      ),
-      awful.button(
-        {},
-        4,
-        function(t)
-          awful.tag.viewnext(t.screen)
-        end
-      ),
-      awful.button(
-        {},
-        5,
-        function(t)
-          awful.tag.viewprev(t.screen)
-        end
-      )
-    ),
-    {},
-    list_update,
-    wibox.layout.fixed.horizontal()
-  )
-end
+  return awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.noempty,
+        buttons = taglist_buttons,
+        style = {},
+        update_function = list_update,
+        layout = wibox.layout.fixed.horizontal()
+    }
+ end
+  -- return awful.widget.taglist(
+  --   s,
+  --   awful.widget.taglist.filter.noempty,
+  --   taglist_buttons,
+    -- gears.table.join(
+    --   awful.button(
+    --     {},
+    --     1,
+    --     function(t)
+    --       t:view_only()
+    --     end
+    --   )
+      -- awful.button(
+      --   { modkey },
+      --   1,
+      --   function(t)
+      --     if client.focus then
+      --       client.focus:move_to_tag(t)
+      --     end
+      --   end
+      -- ),
+      -- awful.button(
+      --   {},
+      --   3,
+      --   function(t)
+      --     if client.focus then
+      --       client.focus:toggle_tag(t)
+      --     end
+      --   end
+      -- ),
+      -- awful.button(
+      --   { modkey },
+      --   3,
+      --   function(t)
+      --     if client.focus then
+      --       client.focus:toggle_tag(t)
+      --     end
+      --   end
+      -- ),
+      -- awful.button(
+      --   {},
+      --   4,
+      --   function(t)
+      --     awful.tag.viewnext(t.screen)
+      --   end
+      -- ),
+      -- awful.button(
+      --   {},
+      --   5,
+      --   function(t)
+      --     awful.tag.viewprev(t.screen)
+      --   end
+      -- )
+    -- ),
+--     {},
+--     list_update,
+--     wibox.layout.fixed.horizontal()
+--   )
+-- end
