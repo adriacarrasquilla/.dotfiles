0bbc4f6cd7c555e36d30219b0473d715147da507
Adri <carrasquilla.adria@gmail.com>
2022-12-09 18:07:17 +0100

    awesome: kblayout widget added

--- awesome/.config/awesome/bar/init.lua
+++ awesome/.config/awesome/bar/init.lua
@@ -53,13 +53,14 @@ awful.screen.connect_for_each_screen(
   s.layoutlist = require("src.widgets.layout_list")()
   
   s.powerbutton = require("src.widgets.power")()
-  -- s.kblayout = require("src.widgets.kblayout")(s)
+  s.kblayout = require("src.widgets.kblayout")(s)
+  -- s.kblayout = nil
   s.taglist = require("src.widgets.taglist")(s)
   s.network = require("src.widgets.network")(s)
   -- s.tasklist = require("src.widgets.tasklist")(s)
   --s.cpu_freq = require("src.widgets.cpu_info")("freq", "average")
 
   require("bar.left_bar")(s, { s.layoutlist, s.taglist })
-  require("bar.right_bar")(s, { s.date, s.clock, s.network, s.audio, s.battery, s.powerbutton })
+  require("bar.right_bar")(s, { s.date, s.clock, s.network, s.kblayout, s.audio, s.battery, s.powerbutton })
 end
 )
