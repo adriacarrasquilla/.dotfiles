b8b1c1d5aa10a6175666e06d7f0268db053f8eb0
Adri <carrasquilla.adria@gmail.com>
2023-01-30 11:41:21 +0100

    Removed deprecated startup nvim tree stuff

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -12,11 +12,6 @@ vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
 
 
 local config = {
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   auto_reload_on_write = false,
   hijack_directories = {
     enable = false,
