f76608581def77a303a6552d1b860e0408648096
Adri <carrasquilla.adria@gmail.com>
2023-02-03 14:29:56 +0100

    dashboard plugin

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,12 +1,11 @@
 return {
     "goolord/alpha-nvim",
     dependencies = {
-        -- "kyazdani42/nvim-web-devicons",
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
       require('alpha').setup(
-            require('alpha.themes.startify').config
+            require('alpha.themes.dashboard').config
         )
     end
 }
