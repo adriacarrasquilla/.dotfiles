9b82bf265bb82b2999840dcb10c1d419169ac5e9
Adri <carrasquilla.adria@gmail.com>
2023-02-08 21:39:21 +0100

    Fixed vimwiki ignoring the path, reverted zsh workaround

--- nvim/.config/nvim/lua/packer-config/init.lua
+++ nvim/.config/nvim/lua/packer-config/init.lua
@@ -15,6 +15,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Lazy needs to have the mapleader set before anything else
 vim.g.mapleader = ' '
 
+-- I need to set up this before the plugin. Otherwise vimwiki will ignore it
+vim.g.vimwiki_list = {{path = '~/Notes/', syntax = 'markdown', ext = '.md'}}
+
 require("lazy").setup('plugins', {ui={border="rounded"}})
 
 require("mason").setup({ui={border="rounded"}})
